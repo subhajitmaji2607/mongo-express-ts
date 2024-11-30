38fc7f9df5b88b0e74d50efaeee7d3b736951e28
subhajit <subhajit.maji@codelogicx.com>
2024-11-30 12:14:47 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import express, { Request, Response , Application } from 'express';
+import Server from './config/server.config'
 import ENVIRONMENT_CONFIG from './config/environment.config.ts';
-import routes from "./routes";
 
-const app: Application = express();
 const PORT = ENVIRONMENT_CONFIG.APP_PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Welcome to Express & TypeScript Server.`);
-});
-
-//Register all application routes
-app.use('/api', routes);
-
-app.listen(PORT, () => {
+Server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
